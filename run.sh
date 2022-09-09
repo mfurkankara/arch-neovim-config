df578a74e530caab486890a905d2f798a96fccf2
mfurkankara <46734065+mfurkankara@users.noreply.github.com>
2022-09-09 12:31:08 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Installing neovim
+sudo pacman -S neovim
+
+# Installing vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       
+# Moving config 
+mv ./config/* ~./config/nvim/
